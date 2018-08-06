35ea166e9f0281ba3dccbb76cc9bb7b77ba9cc65
KIRUTHIKA555 <kiruthikakt38@gmail.com>
2018-08-06 14:31:47 +0530

    Create c.java

---
+++ c.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Number
+{
+public static void main(String args[])
+Scanner sc=new Scanner(System.in);
+int n;
+if(n/2==0)
+{
+System.out.println("Even");
+}
+if(n/2!==0)
+{
+System.out.println("Odd");
+}
+else
+{
+System.out.println("Invalid Input");
+}
+}
