25aacfb3bf2f6f8ac1d8d80a13a189115837ac4c
KIRUTHIKA555 <kiruthikakt38@gmail.com>
2018-08-06 14:51:41 +0530

    Create e.java

---
+++ e.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Letters
+{
+public static void main(String args[])
+char ch;
+if(ch=|A|,|B|,|C|,|D|,|E|,|F|,|G|,|H|,|I|,|J|,|K|,|L|,|M|,|N|,|O|,|P|,|Q|,|R|,|S|,|T|,|U|,|V|,|W|,|X|,|Y|,|Z|)
+{
+System.out.println("Alphabets");
+}
+else
+{
+System.out.println("Not an Alphabet");
+}
+}
+
