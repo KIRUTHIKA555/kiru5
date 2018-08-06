82c84fbe3919f4212397b04a67355e7582aebd3f
KIRUTHIKA555 <kiruthikakt38@gmail.com>
2018-08-06 14:43:49 +0530

    Create d.java

---
+++ d.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class vc
+{
+public static void main(String args[])
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the characters");
+char ch=|A|,|B|,|C|,|D|,|E|,|F|,|G|,|H|,|I|,|J|,|K|,|L|,|M|,|N|,|O|,|P|,|Q|,|R|,|S|,|T|,|U|,|V|,|W|,|X|,|Y|,|Z|;
+if(ch=A,E,I,O,U)
+{
+System.out.println("These are vowels");
+}
+if(ch=B,C,D,F,G,H,J,K,L,M,N,P,Q,R,S,T,V,W,X,Y,Z)
+{
+System.out.print("These are consonants");
+}
+else
+{
+System.out.println("Inavlid");
+}
+}
+}
